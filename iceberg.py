b657d314e564ed91c73ade5a969f4586952116b0
Amir Rachum <amir@rachum.com>
2017-09-10 11:13:27 +0300

    Functionally done.

--- iceberg.py
+++ iceberg.py
@@ -1,11 +1,136 @@
-from typing import List
-from collections import namedtuple
+"""Navigate a ship in the Northern Ocean with icebergs.
+
+Usage:
+    iceberg <input-file> <output-file>
+
+"""
 
 
-Point = namedtuple('Point', 'x y')
+from networkx import Graph, shortest_path
+from typing import List, Tuple, TextIO
+from itertools import combinations
+from sympy.geometry import Point2D, Polygon, Segment2D
+from pathlib import Path
+from docopt import docopt
 
 
-def get_shortest_route(start: Point,
-                       end: Point,
-                       icebergs: List[List[Point]]) -> List[Point]:
-    return [start, end]
+def get_shortest_route(start: Point2D,
+                       end: Point2D,
+                       icebergs: List[Polygon]) -> List[Point2D]:
+    """Calculate the best safe route from `start` to `end`.
+
+    A safe route is one where you don't hit any icebergs.
+
+    """
+    # Create a graph where nodes are all the given points - source,
+    # destination, and all points on all the icebergs.
+    graph = Graph()
+    graph.add_node(start)
+    graph.add_node(end)
+    for iceberg in icebergs:
+        graph.add_nodes_from(iceberg.vertices)
+
+    # For every two nodes on the graph (which are points on the map), we check
+    # if the segment / route between them is blocked. If not, we add a graph
+    # edge between them with the distance as weight.
+    for u, v in combinations(graph.nodes(), r=2):
+        segment = Segment2D(u, v)
+        for iceberg in icebergs:
+            # If we try to travel between adjacent points on an iceberg,
+            # there can't be a collision.
+            if segment in iceberg.sides:
+                continue
+
+            # Since icebergs are convex, any segment between two non-adjacent
+            # points on the iceberg are blocked by the iceberg itself.
+            if u in iceberg.vertices and v in iceberg.vertices:
+                break
+
+            # The intersection is a list of points and segments.
+            intersection = segment.intersection(iceberg)
+            if not intersection:
+                continue
+
+            # If the intersection is the source or the destination point,
+            # then it's okay. It's also fine if we walk alongside the iceberg.
+            if len(intersection) == 1 and (intersection[0] in (u, v) or
+                                           intersection[0] in iceberg.sides):
+                continue
+
+            # In any other case, there's a bad collision.
+            break
+        else:
+            graph.add_edge(u, v, {'distance': u.distance(v)})
+
+    # Now that we have the graph with proper distances, we can use a shortest
+    # path algorithm (e.g., Dijkstra) to find the shortest path.
+    return shortest_path(graph, source=start, target=end, weight='distance')
+
+
+def read_parameters(stream: TextIO) -> Tuple[Point2D, Point2D, List[Polygon]]:
+    """Read lines from an input stream and return the problem parameters.
+
+    The input should be in the following format:
+
+    1. Integer with number of icebergs
+    2. A set of 2D polygons representing icebergs boundaries, represented as
+       a set of ordered coordinates. One line for an iceberg,
+       the format: x1,y1 x2,y2 x3,y3.
+    3. Position of Start point A in the format x,y.
+    4. Position of End point B in the format x,y.
+
+    """
+    def readline():
+        return stream.readline().strip()
+
+    def point_from_str(point_str):
+        x, y = point_str.split(',')
+        return Point2D(int(x), int(y))
+
+    iceberg_num = int(readline())
+    icebergs: List[Polygon] = []
+    for _ in range(iceberg_num):
+        iceberg = Polygon(*[point_from_str(point_str)
+                            for point_str in readline().split()])
+        icebergs.append(iceberg)
+
+    start = point_from_str(readline())
+    end = point_from_str(readline())
+    return start, end, icebergs
+
+
+def write_solution(stream: TextIO, route: List[Point2D]) -> None:
+    """Write the resulting route into the output stream.
+
+    The output should be in the following format:
+
+    1. An ordered list of co­ordinates which combine the selected path from
+       A to B not crossing any iceberg. Including A and B.
+
+    """
+    stream.write(' '.join(f'{point.x},{point.y}' for point in route))
+
+
+def solve_file(input_path: Path, output_path: Path) -> None:
+    """Solve the shortest route problem from the input file.
+
+    The solution is written to the output file.
+
+    """
+    with input_path.open('rt') as input_file:
+        problem_parameters = read_parameters(input_file)  # type: ignore
+
+    solution = get_shortest_route(*problem_parameters)
+
+    with output_path.open('wt') as output_file:
+        write_solution(output_file, solution)  # type: ignore
+
+
+def main():
+    """Run the CLI."""
+    args = docopt(__doc__)
+    solve_file(Path(args['<input-file>']), Path(args['<output-file>']))
+
+
+if __name__ == '__main__':
+    main()
--- test_iceberg.py
+++ test_iceberg.py
@@ -1,44 +1,114 @@
 """Test for the iceberg module."""
 
 
-from iceberg import get_shortest_route
+from iceberg import (
+    get_shortest_route,
+    Point2D,
+    Polygon,
+    read_parameters,
+    write_solution,
+)
+
+from io import StringIO
+import textwrap
 
 
 def test_no_icebergs():
     """Test that with no icebergs, a direct route is taken."""
-    start = (4, 4)
-    end = (12, 80)
+    start = Point2D(4, 4)
+    end = Point2D(12, 80)
     icebergs = ()
     assert get_shortest_route(start=start, end=end, icebergs=icebergs) == [
-        (4, 4),
-        (12, 80),
+        Point2D(4, 4),
+        Point2D(12, 80),
     ]
 
 
 def test_irrelevant_icebergs():
     """Test that if the direct route is not interrupted, it is used."""
-    start = (4, 4)
-    end = (12, 80)
+    start = Point2D(4, 4)
+    end = Point2D(12, 80)
     icebergs = (
-        ((0, 0), (1, 1), (0, 2)),
-        ((11, 20), (15, 50), (15, 20)),
+        Polygon(Point2D(0, 0), Point2D(1, 1), Point2D(0, 2)),
+        Polygon(Point2D(11, 20), Point2D(15, 50), Point2D(15, 20)),
     )
     assert get_shortest_route(start=start, end=end, icebergs=icebergs) == [
-        (4, 4),
-        (12, 80),
+        Point2D(4, 4),
+        Point2D(12, 80),
     ]
 
 
-def test_shortest_route():
-    """Test that the correct path is taken."""
-    start = (4, 4)
-    end = (12, 80)
+def test_route_on_iceberg_side():
+    """Test when the shortest route goes alongside an iceberg."""
+    start = Point2D(4, 4)
+    end = Point2D(12, 80)
+
     icebergs = (
-        ((4, 7), (4, 8), (100, 8), (100, 7)),
+        Polygon(Point2D(4, 7), Point2D(4, 8), Point2D(100, 8),
+                Point2D(100, 7)),
     )
     assert get_shortest_route(start=start, end=end, icebergs=icebergs) == [
-        (4, 4),
-        (4, 8),
-        (12, 80),
+        Point2D(4, 4),
+        Point2D(4, 8),
+        Point2D(12, 80),
     ]
 
+
+def test_route_on_iceberg_point():
+    """Test when the shortest route touches one point of an iceberg."""
+    start = Point2D(0, 0)
+    end = Point2D(5, 5)
+
+    icebergs = (
+        Polygon(Point2D(1, 2), Point2D(1, 3), Point2D(5, 2)),
+    )
+    assert get_shortest_route(start=start, end=end, icebergs=icebergs) == [
+        Point2D(0, 0),
+        Point2D(1, 3),
+        Point2D(5, 5),
+    ]
+
+
+def test_complex_route():
+    start = Point2D(5, 0)
+    end = Point2D(5, 5)
+
+    icebergs = (
+        Polygon(Point2D(4, 1), Point2D(8, 1), Point2D(8, 2), Point2D(4, 2)),
+        Polygon(Point2D(6, 3), Point2D(6, 4), Point2D(2, 4), Point2D(2, 3)),
+    )
+    assert get_shortest_route(start=start, end=end, icebergs=icebergs) == [
+        Point2D(5, 0),
+        Point2D(4, 1),
+        Point2D(4, 2),
+        Point2D(6, 3),
+        Point2D(6, 4),
+        Point2D(5, 5),
+    ]
+
+
+def test_input_parsing():
+    input_stream = StringIO(textwrap.dedent("""\
+        2
+        1,2 1,3 5,2
+        10,20 10,30 50,20
+        0,0
+        100,100
+    """))
+
+    start = Point2D(0, 0)
+    end = Point2D(100, 100)
+    icebergs = [
+        Polygon(Point2D(1, 2), Point2D(1, 3), Point2D(5, 2)),
+        Polygon(Point2D(10, 20), Point2D(10, 30), Point2D(50, 20)),
+    ]
+
+    assert read_parameters(input_stream) == (start, end, icebergs)
+
+
+def test_output_writing():
+    output_stream = StringIO()
+    route = (Point2D(1, 2), Point2D(1, 3), Point2D(5, 2))
+    write_solution(output_stream, route)
+
+    assert output_stream.getvalue() == '1,2 1,3 5,2'
